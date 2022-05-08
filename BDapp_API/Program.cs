8bbbb92e049644984e701304c03bc95c150d0fcb
hatsrog <sebastien-jourdan@laposte.net>
2022-05-08 18:38:57 +0200

    Ajout de swagger UI

--- BDapp_API/Program.cs
+++ BDapp_API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using BDapp_API.Models;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,14 +10,27 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<StockContext>(opt =>
     opt.UseInMemoryDatabase("stockList"));
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
+}
 
 app.Run();
